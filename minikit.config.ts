7d6ad3ab6f135adc79b7d281951b7453c6f873a1
Jamie Gabbay <jamie.gabbay@coinbase.com>
2025-11-22 12:04:05 -0500

    add account association

--- minikit.config.ts
+++ minikit.config.ts
@@ -9,10 +9,10 @@ const ROOT_URL =
  * @see {@link https://docs.base.org/mini-apps/features/manifest}
  */
 export const minikitConfig = {
-  accountAssociation: {  // these will be added in step 5
-    header: "",
-    payload: "",
-    signature: ""
+  accountAssociation: {
+    header: "eyJmaWQiOjEzNzM2MzYsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHgzOTQyZUU5OTZkYTlkMjQ0NkU2NjgyMjI4Qjc0YTVkMDEwNTc5QzAzIn0",
+    payload: "eyJkb21haW4iOiI4MDIxLWd1ZXN0Ym9vay1zaWdtYS52ZXJjZWwuYXBwIn0",
+    signature: "gPIqgyZ4YveWJqQbUruRIgnKp3KKnEg3RhsG+PmgCgwFLy6vBrD98ilayLPgJ5BWdEH+F8LhgPI7QanM2zc0Sxw="
   },
   baseBuilder: {
     ownerAddress: "0xF00EEE6ADb1963452A2c276f4Df4e917A5e46891",
